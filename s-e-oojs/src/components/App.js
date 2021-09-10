b311ac780ec61df4266a4c1da42140be459ccee1
VQuine <01010110.v@gmail.com>
2021-09-10 15:58:14 -0400

    transition from first slide to the next fades background color

--- s-e-oojs/src/components/App.js
+++ s-e-oojs/src/components/App.js
@@ -1,31 +1,63 @@
 class App {
     constructor(){
         this.dom = new Dom() 
-        this.PHASE1()
+        this.phase1()
     }
 
-    PHASE1(){
+    phase1(){
         //GET SOME CSS MATCHED UP FOR PARENT ELEMENT ENCAPSULATED BEAUTY
-        this.dom.body.className = "PHASE1"
+        this.dom.body.className = "phase1"
 
         let etext = "Prayer Box"
-        let eID = "PHASE1GREETING"
+        let eID = "phase1GREETING"
         this.dom.createElementANDAppend( 'DIV', etext, this.dom.mainSection, eID )
         //INJECT THIS CREATION WITH SPECIFICATIONS
 
-        let element = document.querySelector('#PHASE1GREETING')
+        let element = document.querySelector('#phase1GREETING')
         let opacity = 0
-                let fadeAway = setInterval(function() {
-      
-                    if (opacity < 1) {
-                        opacity = opacity + 0.1
-                        element.style.opacity = opacity
-                    } else {
-                        clearInterval(fadeAway)   
-                    }
-                    
-                }, 100)          
+        let fadeIn = setInterval(function() {
+            
+            if (opacity < 1) {
+                opacity = opacity + 0.1
+                element.style.opacity = opacity
+            } else {
+                clearInterval(fadeIn)                             
+                setTimeout(function(){app.phase2()}, 1000)       
+            }         
 
+        }, 100)       
+
+            }
+                
+    phase2(){          
+        // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        this.dom.clearMainSection()
+        //  B L A N K   S L A T E 
+        // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        
+        
+        // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        // H E A D I N G   I N J E C T I O N 
+        let b = this.dom.body
+        b.className = "phase2"
+        
+        let text = "Prayer Box<br>Heading"
+        let eID = "phase2HEADING"
+        
+        let hding = document.querySelector('header')
+        this.dom.createElementANDAppend('DIV', text, hding, eID)
+        // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        
+        let text2 = "Request Prayer"
+        let eID2 = ""
+        let dLoc = this.dom.mainSection        
+        this.dom.createElementANDAppend('DIV', text2, dLoc, eID2)
+
+
+        let text3 = "Pray For Someone Else"
+        let eID3 = ""
+        // let dLoc = this.dom.mainSection        
+        this.dom.createElementANDAppend('DIV', text3, dLoc, eID3)
     }
     
     // phase1(){
